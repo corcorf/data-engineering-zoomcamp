f8feea804fc3249557e856a912df69d0983b97d9
Flann Corcoran <flann@posteo.net>
2022-01-30 19:35:21 +0000

    week2 homework create dags for yellow taxi, FHV and zone data

---
+++ week_2_data_ingestion/homework/airflow/dags/data_ingestion_yellow_taxi.py
@@ -0,0 +1,80 @@
+"""DAG for importing 2021 NY Yellow Taxi Data to Google Cloud Storage."""
+import os
+from datetime import datetime
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+from ingestion_functions import (
+    convert_source_to_parquet,
+    delete_from_gcs,
+    download_source_file_to_gcs,
+)
+
+PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
+BUCKET = os.environ.get("GCP_GCS_BUCKET")
+
+SOURCE_FILENAME = (
+    "yellow_tripdata_{{ data_interval_start.strftime('%Y-%m') }}.csv"
+)
+SOURCE_URL_PREFIX = "https://s3.amazonaws.com/nyc-tlc/trip+data"
+SOURCE_URL = os.path.join(SOURCE_URL_PREFIX, SOURCE_FILENAME)
+LOCAL_PATH = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
+GCS_PATH = "raw/"
+BIGQUERY_DATASET = os.environ.get("BIGQUERY_DATASET", "trips_data_all")
+
+default_args = {
+    "owner": "airflow",
+    "start_date": datetime(2019, 1, 1),
+    "end_date": datetime(2020, 12, 1),
+    "depends_on_past": False,
+    "retries": 1,
+}
+
+# NOTE: DAG declaration - using a Context Manager (an implicit way)
+with DAG(
+    dag_id="data_ingestion_yellow_taxi",
+    schedule_interval="@monthly",
+    default_args=default_args,
+    catchup=True,
+    max_active_runs=1,
+    tags=["dtc-de"],
+) as dag:
+
+    download_dataset_task = PythonOperator(
+        task_id="download_dataset_task",
+        python_callable=download_source_file_to_gcs,
+        op_kwargs={
+            "src_url": SOURCE_URL,
+            "bucket": BUCKET,
+            "gcs_path": GCS_PATH,
+        },
+    )
+
+    format_to_parquet_task = PythonOperator(
+        task_id="format_to_parquet_task",
+        python_callable=convert_source_to_parquet,
+        op_kwargs={
+            "src_filename": SOURCE_FILENAME,
+            "bucket": BUCKET,
+            "gcs_path": GCS_PATH,
+        },
+    )
+
+    delete_source_task = PythonOperator(
+        task_id="delete_source_task",
+        python_callable=delete_from_gcs,
+        op_kwargs={
+            "src_filename": SOURCE_FILENAME,
+            "bucket": BUCKET,
+            "gcs_path": GCS_PATH,
+        },
+    )
+
+    (
+        download_dataset_task
+        >> format_to_parquet_task
+        >> [
+            delete_source_task,
+        ]
+    )
--- week_2_data_ingestion/homework/airflow/dags/ingestion_functions.py
+++ week_2_data_ingestion/homework/airflow/dags/ingestion_functions.py
@@ -1,25 +1,5 @@
-"""DAG for importing 2021 NY Yellow Taxi Data to Google Cloud Storage."""
-import logging
+"""Functions common to data ingestion DAGs."""
 import os
-from datetime import datetime
-
-from airflow import DAG
-from airflow.operators.python import PythonOperator
-from airflow.providers.google.cloud.operators.bigquery import (
-    BigQueryCreateExternalTableOperator,
-)
-
-PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
-BUCKET = os.environ.get("GCP_GCS_BUCKET")
-
-SOURCE_FILENAME = (
-    "yellow_tripdata_{{ data_interval_start.strftime('%Y-%m') }}.csv"
-)
-SOURCE_URL_PREFIX = "https://s3.amazonaws.com/nyc-tlc/trip+data"
-SOURCE_URL = os.path.join(SOURCE_URL_PREFIX, SOURCE_FILENAME)
-LOCAL_PATH = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
-GCS_PATH = "raw"
-BIGQUERY_DATASET = os.environ.get("BIGQUERY_DATASET", "trips_data_all")
 
 
 def download_source_file(src_url, local_path):
@@ -45,6 +25,8 @@ def format_to_parquet(local_path, src_filename):
 
     :param src_filename: filename for csv source (string)
     """
+    import logging
+
     import pyarrow.csv as pv
     import pyarrow.parquet as pq
 
@@ -85,8 +67,7 @@ def upload_to_gcs(bucket, gcs_path, local_path, filename):
 def download_source_file_to_gcs(src_url, bucket, gcs_path):
     """Download a csv file from a url and save to google cloud storage.
 
-    :param src_url_prefix: beginning of url to target file
-    :param src_filename: filename for csv source (string)
+    :param src_url: url of source csv (string)
     :param bucket: Google Cloud Storage Bucket to save csv (string)
     :param gcs_path: path in Google Cloud Storage Bucket to save csv (string)
     """
@@ -119,6 +100,8 @@ def convert_source_to_parquet(src_filename, bucket, gcs_path):
     :param object_name: target path & file-name
     :return:
     """
+    import logging
+
     if not src_filename.endswith(".csv"):
         logging.error("Can only accept source files in CSV format")
         return
@@ -151,78 +134,17 @@ def convert_source_to_parquet(src_filename, bucket, gcs_path):
         dst_blob.upload_from_filename(tmp_dst)
 
 
-default_args = {
-    "owner": "airflow",
-    "start_date": datetime(2021, 1, 1),
-    "depends_on_past": False,
-    "retries": 1,
-}
+def delete_from_gcs(src_filename, bucket, gcs_path):
+    """Delete a file from google cloud storage.
 
-# NOTE: DAG declaration - using a Context Manager (an implicit way)
-with DAG(
-    dag_id="data_ingestion_gcs_dag",
-    schedule_interval="@monthly",
-    default_args=default_args,
-    catchup=True,
-    max_active_runs=1,
-    tags=["dtc-de"],
-) as dag:
+    :param src_filename: name of file for deletion (string)
+    :param bucket: Google Cloud Storage Bucket to save csv (string)
+    :param gcs_path: path in Google Cloud Storage Bucket to save csv (string)
+    """
+    from google.cloud import storage
 
-    download_dataset_task = PythonOperator(
-        task_id="download_dataset_task",
-        python_callable=download_source_file,
-        op_kwargs={
-            "src_url": SOURCE_URL,
-            "local_path": LOCAL_PATH,
-        },
-    )
-
-    format_to_parquet_task = PythonOperator(
-        task_id="format_to_parquet_task",
-        python_callable=format_to_parquet,
-        op_kwargs={
-            "local_path": LOCAL_PATH,
-            "src_template": SOURCE_FILENAME,
-        },
-    )
-
-    # TODO: Homework - research and try XCOM to communicate output values between 2 tasks/operators
-    local_to_gcs_task = PythonOperator(
-        task_id="local_to_gcs_task",
-        python_callable=upload_to_gcs,
-        op_kwargs={
-            "bucket": BUCKET,
-            "gcs_path": "raw/",
-            "local_path": LOCAL_PATH,
-            "fn_template": SOURCE_FILENAME.replace(".csv", ".parquet"),
-        },
-    )
-
-    bigquery_external_table_task = BigQueryCreateExternalTableOperator(
-        task_id="bigquery_external_table_task",
-        table_resource={
-            "tableReference": {
-                "projectId": PROJECT_ID,
-                "datasetId": BIGQUERY_DATASET,
-                "tableId": "external_table",
-            },
-            "externalDataConfiguration": {
-                "sourceFormat": "PARQUET",
-                "sourceUris": [
-                    os.path.join(
-                        "gs://",
-                        BUCKET,
-                        GCS_PATH,
-                        SOURCE_FILENAME.replace(".csv", ".parquet"),
-                    )
-                ],
-            },
-        },
-    )
-
-    (
-        download_dataset_task
-        >> format_to_parquet_task
-        >> local_to_gcs_task
-        >> bigquery_external_table_task
-    )
+    client = storage.Client()
+    bucket = client.bucket(bucket)
+    object_name = os.path.join(gcs_path, src_filename)
+    blob = bucket.blob(object_name)
+    blob.delete()
